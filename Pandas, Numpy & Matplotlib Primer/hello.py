40faa3e396940a21a2e7f2d6518ac8219c483484
Mutabazi Noble <60974514+Mnoble-19@users.noreply.github.com>
2023-12-22 19:41:36 +0300

    Deep learning practice

--- Pandas, Numpy & Matplotlib Primer/hello.py
+++ Pandas, Numpy & Matplotlib Primer/hello.py
@@ -1,1 +1,4 @@
-print('hello')
+#uploading csv to huggingface
+from datasets import Dataset
+airqo_data = Dataset.from_csv("/Users/noble/Downloads/airqo_data.csv")
+airqo_data.push_to_hub("airqo_data")
